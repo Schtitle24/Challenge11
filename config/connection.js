bc4de3d7dc683387449b3e983bb51cb170c5ca1b
Schtitle <Steidley.winston@gmail.com>
2024-02-06 17:32:42 -0600

    Model refactors and columns defined

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// Check for JAWSDB_URL environment variable, use it if available, otherwise use local MySQL
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      port: 3306
     });
 
 module.exports = sequelize;
